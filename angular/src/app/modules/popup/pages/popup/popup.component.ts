714121f80ab050c74a8de7fb1650da55a8fbaa90
James <ptrsnja@gmail.com>
2020-08-22 15:33:23 -0400

    Icon updates on state change. Basic popup

--- angular/src/app/modules/popup/pages/popup/popup.component.ts
+++ angular/src/app/modules/popup/pages/popup/popup.component.ts
@@ -1,27 +1,38 @@
-import { Component, Inject } from '@angular/core';
-import { bindCallback } from 'rxjs';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { bindCallback, Subject, BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TAB_ID } from '../../../../providers/tab-id.provider';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-popup',
   templateUrl: 'popup.component.html',
   styleUrls: ['popup.component.scss']
 })
-export class PopupComponent {
-  message: string;
+export class PopupComponent implements OnInit, OnDestroy {
+  
+  private readonly onDestroy$: Subject<void>
+  ipCtrl: FormControl;
 
-  constructor(@Inject(TAB_ID) readonly tabId: number) {}
-
-  async onClick(): Promise<void> {
-    this.message = await bindCallback<string>(chrome.tabs.sendMessage.bind(this, this.tabId, 'request'))()
-      .pipe(
-        map(msg =>
-          chrome.runtime.lastError
-            ? 'The current page is protected by the browser, goto: https://www.google.nl and try again.'
-            : msg
-        )
-      )
-      .toPromise();
+  constructor(@Inject(TAB_ID) readonly tabId: number) {
+    this.onDestroy$ = new Subject<void>();
+    this.ipCtrl = new FormControl('');
   }
+
+  ngOnInit() {
+    chrome.runtime.sendMessage({type: 'getLightIp'}, (address) => {
+      this.ipCtrl.setValue(address);
+    });
+  }
+
+  saveIpAddress() {
+    chrome.runtime.sendMessage({type: 'setLightIp', address: this.ipCtrl.value}, (resp) => {
+      console.log('state', resp);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
+
 }
--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PopupComponent } from './pages/popup/popup.component';
 import { PopupRoutingModule } from './popup-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PopupComponent],
-  imports: [CommonModule, PopupRoutingModule]
+  imports: [CommonModule, PopupRoutingModule, ReactiveFormsModule, FormsModule],
+  exports: [FormsModule, ReactiveFormsModule]
 })
 export class PopupModule {}
